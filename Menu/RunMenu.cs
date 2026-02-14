06493634b3a42348659dd9d2a28f28f2ae489f49
Kristofer Sell <kristoferelissell@gmail.com>
2026-02-14 22:43:59 +0100

    Created SubMenu.cs and moved the choice functions there

--- Menu/RunMenu.cs
+++ Menu/RunMenu.cs
@@ -40,22 +40,22 @@ public class RunMenu
             switch (input)
             {
                 case "1":
-                    GetAverageTempForSelectedDate(data,location);
+                    SubMenu.GetAverageTempForSelectedDate(data,location);
                     break;
                 case "2":
-                    WarmestToColdest(data, location);
+                    SubMenu.WarmestToColdest(data, location);
                     break;
                 case "3":
-                    DryestToMoistiest(data, location);
+                    SubMenu.DryestToMoistiest(data, location);
                     break;
                 case "4":
-                    SortByMoldRisk(data,location);
+                    SubMenu.SortByMoldRisk(data,location);
                     break;
                 case "5" when location == "Ute":
-                    CheckForStreakBelowTemperature(data, location,  10);
+                    SubMenu.CheckForStreakBelowTemperature(data, location,  10);
                     break;
                 case "6" when location == "Ute":
-                    CheckForStreakBelowTemperature(data, location,0);
+                    SubMenu.CheckForStreakBelowTemperature(data, location,0);
                     break;
                 case "0":
                     isRunningSubMenu = !isRunningSubMenu;
@@ -63,117 +63,7 @@ public class RunMenu
             }
         }
     }
-    private static void GetAverageTempForSelectedDate(List<WeatherDataProperties> data,string location)
-    {
-        // Välj Datum mellan 2016-05-31 till 2017-01-10
-        Console.Clear();
-        Console.Write("Ange ett datum: [åååå-mm-dd] \n");
-        string input = Console.ReadLine();
-        if (DateTime.TryParse(input, out DateTime searchedDate))
-        {
-            var selectedDateBySearch = Helper.GetStatistics(data,location).FirstOrDefault(x => x.Date == searchedDate);
-
-            if (selectedDateBySearch !=null)
-            {
-
-                Console.WriteLine($"Statistik enligt input: {searchedDate:yyyy-MM-dd}");
-                Console.WriteLine($"Medeltemperatur: {selectedDateBySearch.AverageTemperature:F1} C°");
-                Console.WriteLine($"Medelfuktighet: {selectedDateBySearch.AverageHumidity:F1}%");
-            }
-            else
-            {
-                Console.WriteLine("Det finns ingen data på valt datum! ");
-            }
-        }
-        else
-        {
-            Console.WriteLine("Du skrev felaktigt format. Prova 'åååå-mm-dd' ! ");
-        }
-
-        Helper.PressToContinue("Tryck på valfri tangent för att fortsätta...");
-    }
-    private static void WarmestToColdest(List<WeatherDataProperties> data, string location)
-    {
-        Console.Clear();
-        var fullResult = Helper.GetStatistics(data, location).OrderByDescending(x => x.AverageTemperature);
-     
-        Console.WriteLine($"Medeltemperaturen och Datum ({location}):");
-        foreach (var item in fullResult)
-        {
-            Console.WriteLine(
-                $"{item.Date:yyyy-MM-dd} {item.AverageTemperature:F1}°C - {item.Location}");
-        }
-
-        Helper.PressToContinue("Tryck på valfri tangent för att fortsätta...");
-    }
-    private static void DryestToMoistiest(List<WeatherDataProperties> data, string location)
-    {
-        Console.Clear();
-        var fullResult = Helper.GetStatistics(data, location).OrderBy(x => x.AverageHumidity);
-
-        Console.WriteLine("Medelfuktighet och Datum");
-        foreach (var item in fullResult)
-        {
-            Console.WriteLine($"{item.Date:yyyy-MM-dd} {item.AverageHumidity:F0}");
-        }
-
-        Helper.PressToContinue("Tryck på valfri tangent för att fortsätta...");
-    }
-    private static void SortByMoldRisk(List<WeatherDataProperties> data,string location)
-    {
-        Console.Clear();
-        var fullResult = Helper.GetStatistics(data,location).OrderByDescending(x => x.AverageMold);
-        
-        Console.WriteLine("Moldrisk from bottom to top and date");
-        foreach (var item in fullResult)
-        {
-            Console.WriteLine($"{item.Date:yyyy-MM-dd} {item.AverageMold:F1}");
-        }
-
-        Helper.PressToContinue("Tryck på valfri tangent för att fortsätta...");
-        
-    }
-    private static void CheckForStreakBelowTemperature(List<WeatherDataProperties> data,string location,double temperature)
-    {
-        
-         var fullResult = Helper.GetStatistics(data, location).OrderBy(x => x.Date);
-         int consecutiveDays = 0;
-         DateTime? streakStartDate = null;
-         foreach (var streak in fullResult)
-         {
-             if (streak.AverageTemperature <= temperature)
-             {
-                 if (consecutiveDays == 0)
-                 {
-                     streakStartDate = streak.Date;
-                 }
-                 consecutiveDays++;
-                 Console.WriteLine(
-                     $"{streak.Date:yyyy-MM-dd}: Dag: {consecutiveDays}, Medeltemperatur: {streak.AverageTemperature:F1}C°");
-                 if (consecutiveDays == 5)
-                 {
-                     DateTime resultDate = streak.Date.AddDays(-4);
-                     Console.WriteLine($"Vi har hittat dagen. {streakStartDate:yyyy-MM-dd} Avslutar räkningen. Streaken började vid {resultDate:yyyy-MM-dd}");
-                     break;
-                 }
-                  
-             }
-             else
-             {
-                 consecutiveDays = 0;
-                 streakStartDate = null;
-             }
-             
-             
-         }
-         if (consecutiveDays < 5)
-         {
-             Console.WriteLine($"Ingen 5-dagarsperiod under {temperature}°C kunde hittas");
-         }
-         
-         Helper.PressToContinue("Tryck på valfri tangent för att fortsätta...");
-
-    }
+    
     
 }
 
