e9212a90715f22fdb333454ccc2af03dc8996950
elikrisel <72739974+elikrisel@users.noreply.github.com>
2026-09-17 18:28:06 +0200

    Experimentign with PR on CLI

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Test a new line in comment
+        //Test a new line in comment. Creating PR
         List<WeatherDataProperties> weatherDataList = LoadWeatherData.WeatherData("test.txt");
         RunMenu.RunApplication(weatherDataList);
         
